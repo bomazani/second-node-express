7f8f962a208eafd61ffdd2039416627cbffd3317
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-18 12:31:03 -0400

    Added a basic POST form.

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,64 @@
 const express = require('express')
 const router = express.Router()
 
+// POST, GET, PUT, DELETE
+
 router.get('/', (req, res) => {
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
-router.get('/query',(req, res)=> {
-	const name = req.query.name
+router.post('/post', (req, res) => {
+	const body = req.body //normally comes from a POST form
 
 	res.json({
-		name: name
+		confirmation: 'success',
+		data: body
 	})
 })
 
-router.get('/:path',(req, res)=> {
-	const path = req.params.path
+// router.get('/query',(req, res)=> {
+// 	const name = req.query.name
+//	const occupation = req.query.occupation
 
-	res.json({
-		data: path
-	})
-})
+// 	res.json({
+// 		title: title
+// 	})
+// })
 
-router.get('/:profile/:username',(req, res)=> {
+
+// router.get('/:path',(req, res)=> {
+// 	const path = req.params.path
+
+// 	res.json({
+// 		data: path
+// 	})
+// })
+
+// Request Parameters
+router.get('/:profile/:username', (req, res, next) => {
 	const profile = req.params.profile
 	const username = req.params.username
-
 
 	res.json({
 		profile: profile,
 		username: username
 	})
+})
+
+// Request Query Parameters
+router.get('/query',(req, res)=> {
+	const name = req.query.name
+	const job = req.query.job
+	const title = req.query.title
+
+	const data = {
+		name: name,
+		job: job,
+		title: title
+	}
+
+	res.render('profile', data)
+
 })
 
 module.exports = router
