d92508360b910b6e8ce7b8c3ed14785e7f31f145
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-17 23:39:08 -0400

    Added a variable 'path' to the route in routes/index.js to enable various paths to be entered into the url.

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,12 @@ router.get('/', (req, res) => {
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
+router.get('/:path',(req, res)=> {
+	const path = req.params.path
+
+	res.json({
+		data: 'This is a test response.'
+	})
+})
 
 module.exports = router
