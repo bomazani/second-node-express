9bc601fbcff40b269b352fe09918e53511b8f01a
Bob Hunt <bob.hunt.email@gmail.com>
2020-05-18 14:00:41 -0400

    Defined 'body' in register.js to resolve error.

--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/user', (req, res) => {
+    const body = req.body
 
     res.json({
         confirmation: 'success',
